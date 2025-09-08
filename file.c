90f381b0b6245cc35f8e99e0f38c5a995e632c2f
varshini-537 <mleelasriharshavardhan_cse230599@mgit.ac.in>
2025-09-08 10:34:46 +0530

    initial commit

---
+++ file.c
@@ -0,0 +1,16 @@
+#include<>[Dstdio.h[>
+void main()
+{
+int n;
+printf()[D""[DEnter any number");
+scanf()[D""[D%d[",&n);
+if()[Dn<=0)
+{}[D
+[A[Cprintf()[D""[DThe Entered number is positive");
+}
+else
+{printf()[D""[DThe Entered number is negative");
+}
+[A[A[C
+[C
+}ls
